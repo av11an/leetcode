f951af0a96869f3092af3b86a2204301308b911e
Evan Ellis <portalevan@gmail.com>
2023-01-28 23:35:32 -0800

    start of problem

---
+++ Easy - Roman To Integer/RomanToInt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class RomanToInt {
+
+    public static int romanToInt(String s){
+        return 1;
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String input = scn.nextLine();
+        System.out.println(romanToInt(input));
+        scn.close();
+    }
+}
